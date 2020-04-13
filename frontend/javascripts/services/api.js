a42bdc1b72dbc07a9caf3ff3b1c0ec8ba349cc9d
Noah Scala <nwscala@gmail.com>
2020-04-13 16:29:15 -0400

    added api class to handle fetch requests

---
+++ frontend/javascripts/services/api.js
@@ -0,0 +1,26 @@
+class API {
+    static baseURL = "http://localhost:3000"
+
+    static get(url) {
+        return fetch(this.baseURL + url)
+            .then(function(response) {
+                if (response.status !== 200) {
+                    throw new Error(response.statusText)
+                }
+
+                return response.json()
+            })
+    }
+
+    static post(url, data) {
+        return fetch(this.baseURL + url, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.json())
+    }
+}
